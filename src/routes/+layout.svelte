f387fcf39d6b944e9c188dc8e55d8e3251a6ba6d
Psalm Diao <p.diao25@gmail.com>
2025-01-01 21:21:00 +0100

    Showcase bird pictures in photo gallery

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,23 +4,36 @@
 </script>
 
 <Sidebar />
-<main>
-  {@render children()}
-</main>
+
+<div class="wrapper">
+  <main>
+    {@render children()}
+  </main>
+
+  <footer>
+    <hr class="dashed" />
+    <a href="/legal-notice/en" class="legal-notice">Legal notice (Impressum)</a>
+  </footer>
+</div>
 
 <style lang="scss">
   :root {
+    --light-grey: #E0E1E1;
     --dark-grey: #3b3b3b;
     --orange: #FFAD05;
     --orange-light: #FFD685;
   }
 
-  main {
+  div.wrapper {
     flex-grow: 2;
     padding: 2.5em 0 5.5em 2em;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
-  :global(a:not(.nav-link)) {
+  :global(a:not(.nav-link):not(.legal-notice)) {
     color: var(--dark-grey);
     text-decoration: underline;
     text-decoration-color: var(--orange-light);
@@ -37,8 +50,25 @@
     }
   }
 
+  :global(hr.dashed) {
+    border: 0.08rem dashed var(--light-grey);
+  }
+
+  footer {
+    margin-top: 3rem;
+
+    a.legal-notice {
+      font-size: 9pt;
+      color: var(--dark-grey);
+      letter-spacing: 0.1rem;
+      &:visited {
+        color: var(--dark-grey);
+      }
+    }
+  }
+
   @media screen and (max-width: 790px) {
-    main {
+    div.wrapper {
       flex-grow: 0 0 auto;
       padding: 1.5rem 2rem 4rem;
     }
--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,1 +1,41 @@
-Todo!
+<script>
+  import License from "../../lib/components/License.svelte";
+  import wren from "../../lib/img/p-wren-20240225.webp";
+  import siskin from "../../lib/img/p-siskin-20240127.webp";
+  import longtailedtit from "../../lib/img/p-longtailedtit-202403.webp";
+</script>
+
+<p>As much as I enjoy perceiving birds from an ornithological viewpoint, I also like taking pictures of them. My equipment isn&#8217;t the newest: I use a Canon EOS 70D and a Sigma 150-500 mm 5-6.3 DG OS APO HSM. Consecutively, I often (have to) do some finishing touches during post-processing.</p>
+<p>Here are some bird pictures I took in 2024. Do note that they are compressed to the <code>.webp</code> file format.</p>
+
+<figure>
+  <img src={longtailedtit} alt="" />
+  <figcaption>A long-tailed tit on a thin branch with some nesting material in its beak</figcaption>
+</figure>
+
+<figure>
+  <img src={siskin} alt="" />
+  <figcaption>A Eurasian siskin on an alder branch against a sunlit background</figcaption>
+</figure>
+
+<figure>
+  <img src={wren} alt="" />
+  <figcaption>A Eurasian wren on a mossy log</figcaption>
+</figure>
+
+<License type="cc-by-sa" />
+
+<style>
+  figure {
+    margin: 2.5rem 0.75rem;
+    padding: 0;
+    img {
+      width: 100%;
+    }
+    figcaption {
+      font-size: 11pt;
+      text-align: center;
+      padding: 0 2.5rem;
+    }
+  }
+</style>
